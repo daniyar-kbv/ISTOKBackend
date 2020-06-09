37af7f3dca337c194d73810a4733e488b9f547f2
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-06-09 16:22:24 +0600

    type to subtype added

--- tests/serializers.py
+++ tests/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from main.models import Project
+from main.models import Project, ProjectDocument
 
 
 class ProjectCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+
+    def create(self, validated_data):
+        project = Project.objects.create(**validated_data)
+
+        documents = self.context['documents']
+        if documents:
+            for document in documents:
+                ProjectDocument.objects.create(project=project, document=document)
--- tests/views.py
+++ tests/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 from tests.serializers import ProjectCreateSerializer
 from main.models import Project
 
@@ -7,3 +8,14 @@ class ProjectViewSet(viewsets.GenericViewSet,
                      mixins.CreateModelMixin):
     serializer_class = ProjectCreateSerializer
     queryset = Project.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        documents = request.data.getlist('documents')
+        context = {
+            'documents': documents
+        }
+        serializer = self.get_serializer(data=request.data, context=context)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
