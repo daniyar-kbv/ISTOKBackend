33bbf0796a651fc90baaf571eee8dbe194bf198f
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-06-09 17:18:05 +0600

    tests

--- istok/settings.py
+++ istok/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "api/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.MainUser'
