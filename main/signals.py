77315443e733057dd472141193e49de8d0e332aa
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-06-09 16:59:31 +0600

    tests

--- main/signals.py
+++ main/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from main.models import Project
+from main.models import ProjectDocument
 from utils import upload
 
 
-@receiver(post_delete, sender=Project)
+@receiver(post_delete, sender=ProjectDocument)
 def order_picture_deleted(sender, instance, created=True, **kwargs):
     upload.delete_file(instance.document)
