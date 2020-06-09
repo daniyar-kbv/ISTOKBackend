e818887a2c39839f7f8f65978e6bf9478e13a570
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-06-09 18:10:16 +0600

    tests

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework import routers
-from tests.views import ProjectViewSet
+from tests.views import ProjectViewSet, BlogPostViewSet
 
 urlpatterns = [
 ]
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
+router.register('blogs', BlogPostViewSet)
 
 urlpatterns += router.urls
