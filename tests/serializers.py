5bcee58c1865a86d145c9ed9bb281baa177509c8
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-06-09 18:09:40 +0600

    tests

--- tests/serializers.py
+++ tests/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from main.models import Project, ProjectDocument
+from blog.models import BlogPost, PostDocument
 
 
 class ProjectCreateSerializer(serializers.ModelSerializer):
@@ -23,3 +24,19 @@ class ProjectCreateSerializer(serializers.ModelSerializer):
             for document in documents:
                 ProjectDocument.objects.create(project=project, document=document)
         return project
+
+
+class BlogPostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+
+    def create(self, validated_data):
+        post = BlogPost.objects.create(**validated_data)
+
+        documents = self.context.get('documents')
+
+        if documents:
+            for document in documents:
+                PostDocument.objects.create(post=post, document=document)
+        return post
--- tests/views.py
+++ tests/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
-from tests.serializers import ProjectCreateSerializer
+from tests.serializers import ProjectCreateSerializer, BlogPostCreateSerializer
 from main.models import Project
+from blog.models import BlogPost
 
 
 class ProjectViewSet(viewsets.GenericViewSet,
@@ -11,11 +12,26 @@ class ProjectViewSet(viewsets.GenericViewSet,
 
     def create(self, request, *args, **kwargs):
         documents = request.data.getlist('documents')
-        print(documents)
         context = {
             'documents': documents
         }
         serializer = ProjectCreateSerializer(data=request.data, context=context)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class BlogPostViewSet(viewsets.GenericViewSet,
+                      mixins.CreateModelMixin):
+    queryset = BlogPost.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        documents = request.data.getlist('documents')
+        context = {
+            'documents': documents
+        }
+        serializer = BlogPostCreateSerializer(data=request.data, context=context)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
