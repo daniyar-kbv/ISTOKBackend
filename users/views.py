c3d5dc802894210aef88f05f5b801b40ebfbed77
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-06-25 15:02:02 +0600

    hotfix

--- users/views.py
+++ users/views.py
@@ -220,7 +220,7 @@ class UserViewSet(viewsets.GenericViewSet,
             return redirect('https://docs.djangoproject.com/en/3.0/topics/http/shortcuts/')
         # TODO: if not active
         logger.error(f'Email verification ({email}): failed')
-        return redirect(f'https://istokhome.com/registration-specials?email={email}')
+        return redirect(f'https://istokhome.com/registration-specials?role={activation.role}&email={email}')
 
     @action(detail=False, methods=['post'])
     def verify_phone(self, request, pk=None):
