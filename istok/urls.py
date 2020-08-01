80129b07381adab0ed6974eba2e84f7211d001aa
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-08-01 13:11:55 +0600

    nested inline changed

--- istok/urls.py
+++ istok/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = i18n_patterns(
     url(r'^api/admin/', admin.site.urls),
@@ -31,7 +30,7 @@ urlpatterns = i18n_patterns(
     prefix_default_language=False
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
     static(settings.ADDITIONAL_URL, document_root=settings.ADDITIONAL_ROOT) + \
-    staticfiles_urlpatterns()
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.index_title = ''
 admin.site.site_header = 'Панель администрирования ISTOK HOME'
